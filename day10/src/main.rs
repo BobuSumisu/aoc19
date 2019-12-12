cbf0453470752625d6c20cd04fff838fc6e975d8
Øyvind Ingvaldsen <oyvind.ingvaldsen@gmail.com>
2019-12-12 17:05:36 +0100

    finally a solution to day10 that works...

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,135 +1,195 @@
-use std::collections::HashSet;
-use std::fs;
-use std::mem;
+use itertools::Itertools;
+use std::cmp::Ordering;
+use std::collections::HashMap;
+use std::f64::consts::PI;
+use std::f64::EPSILON;
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Copy, Clone, PartialEq, Eq, Debug, Hash)]
 struct Point {
-    x: f64,
-    y: f64,
+    x: i64,
+    y: i64,
 }
 
 impl Point {
-    const fn new(x: f64, y: f64) -> Self {
+    const fn new(x: i64, y: i64) -> Self {
         Self { x, y }
     }
 
-    fn quad(&self, other: &Self) -> i64 {
-        if other.x >= self.x {
-            if other.y >= self.y {
-                1
-            } else {
-                4
-            }
+    fn bearing(&self, other: &Self) -> f64 {
+        let theta = ((other.x - self.x) as f64).atan2((other.y - self.y) as f64);
+        let degrees = 180.0 - theta.to_degrees();
+
+        if degrees < 0.0 {
+            degrees + 180.0
         } else {
-            if other.y >= self.y {
-                2
-            } else {
-                3
-            }
+            degrees
         }
     }
 
-    fn slope(&self, other: &Self) -> f64 {
-        (other.y - self.y) / (other.x - self.x)
-    }
-
-    fn dist(&self, other: &Self) -> f64 {
-        ((other.x - self.x).powi(2) + (other.y - self.y).powi(2)).sqrt()
-    }
-}
-
-impl PartialEq for Point {
-    fn eq(&self, other: &Self) -> bool {
-        (self.x - other.x).abs() < 0.0001 && (self.y - other.y).abs() < 0.0001
+    fn distance(&self, other: &Self) -> f64 {
+        let x = (other.x - self.x) as f64;
+        let y = (other.y - self.y) as f64;
+        (x * x + y * y).sqrt()
     }
 }
 
 #[derive(Debug)]
-struct Space {
-    asteroids: Vec<Point>,
+struct RelPoint {
+    point: Point,
+    bearing: f64,
+    distance: f64,
 }
 
-impl Space {
-    fn can_see(&self, a: &Point, b: &Point) -> bool {
-        if a == b {
-            return false;
+impl RelPoint {
+    fn new(point: &Point, origin: &Point) -> Self {
+        Self {
+            point: *point,
+            bearing: origin.bearing(point),
+            distance: origin.distance(point),
         }
+    }
+}
 
-        // Yeaa, so the idea here is iterating through every asteroid and check
-        // if they are in front of `b`. This is done by simply checking if `b` and `c` are in
-        // the same quadrant relative to `a` and that they have the same slope.
-        // My thought was if so, and if `c` is closer than `b`, then `c` is blocking the view from
-        // `a` to `b`.
-        //
-        // This is not very effective: for every asteroid `a` you have to check if it can see every
-        // asteroid `b` which required checking if any asteroid `c` is in between.
-        //
-        // So I guess worst runtime would be `n^3` :P
-        let quad = a.quad(b);
-        let slope = a.slope(b);
-        let dist = a.dist(b);
+fn view_count(points: &[Point], point: &Point) -> usize {
+    let mut count = 0;
 
-        for c in &self.asteroids {
-            if c == a || c == b {
+    let rel_points: Vec<RelPoint> = points
+        .iter()
+        .filter_map(|p| {
+            if p == point {
+                None
+            } else {
+                Some(RelPoint::new(p, point))
+            }
+        })
+        .collect();
+
+    'outer: for target in &rel_points {
+        for blocker in &rel_points {
+            if blocker.point == target.point {
                 continue;
             }
 
-            if a.quad(c) == quad && a.slope(c) == slope && a.dist(c) < dist {
-                return false;
+            if target.bearing == blocker.bearing && blocker.distance < target.distance {
+                continue 'outer;
             }
         }
 
-        true
+        count += 1;
     }
 
-    fn find_best(&self) -> usize {
-        let mut best = 0;
-
-        for a in &self.asteroids {
-            let mut can_see = 0;
-
-            for b in &self.asteroids {
-                if self.can_see(a, b) {
-                    can_see += 1;
-                }
-            }
-
-            if can_see > best {
-                best = can_see
-            }
-        }
-
-        best
-    }
+    count
 }
 
-fn parse_input(input: &str) -> Space {
-    let asteroids = input
-        .lines()
-        .enumerate()
-        .flat_map(|(y, line)| {
-            line.chars()
-                .enumerate()
-                .filter_map(|(x, c)| {
-                    if c == '#' {
-                        Some(Point::new(x as f64, y as f64))
-                    } else {
-                        None
-                    }
-                })
-                .collect::<Vec<Point>>()
+fn find_best(points: &[Point]) -> (Point, usize) {
+    let mut best_count = 0;
+    let mut best = points[0];
+
+    for point in points {
+        let count = view_count(points, point);
+        if count > best_count {
+            best_count = count;
+            best = *point;
+        }
+    }
+
+    (best, best_count)
+}
+
+fn vaporize(points: &[Point], point: &Point, max: usize) -> Vec<Point> {
+    // Augment with bearing and distance information (and remove point).
+    let mut rel_points: Vec<RelPoint> = points
+        .iter()
+        .filter_map(|p| {
+            if p == point {
+                None
+            } else {
+                Some(RelPoint::new(p, point))
+            }
         })
         .collect();
-    Space { asteroids }
+
+    // Group points by bearing and sort by distance.
+    // Must sort first since group_by only looks at consecutive elements.
+
+    rel_points.sort_by(|p0, p1| {
+        if p0.bearing < p1.bearing {
+            Ordering::Less
+        } else {
+            Ordering::Greater
+        }
+    });
+
+    let mut grouped: Vec<Vec<_>> = rel_points
+        .iter()
+        .group_by(|p| p.bearing)
+        .into_iter()
+        .map(|(_, g)| g.into_iter().collect::<Vec<&RelPoint>>())
+        .collect();
+
+    // Now sort each group by distance.
+    for group in grouped.iter_mut() {
+        group.sort_by(|p0, p1| {
+            if p0.distance < p1.distance {
+                Ordering::Less
+            } else {
+                Ordering::Greater
+            }
+        });
+    }
+
+    // Now we can start the show by simply looping over the groups and removing the first element.
+    let mut vaporized = vec![];
+
+    while vaporized.len() < max {
+        let mut found = false;
+
+        for group in grouped.iter_mut() {
+            if !group.is_empty() {
+                let rel_point = group.remove(0);
+                vaporized.push(rel_point.point);
+                found = true;
+
+                if vaporized.len() == max {
+                    return vaporized;
+                }
+            }
+        }
+
+        if !found {
+            break;
+        }
+    }
+
+    vaporized
 }
 
-fn get_input() -> Space {
-    parse_input(&fs::read_to_string("input/input.txt").unwrap())
+fn parse_input(input: &str) -> Vec<Point> {
+    input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, l)| {
+            l.chars().enumerate().filter_map(move |(x, c)| {
+                if c == '#' {
+                    Some(Point::new(x as i64, y as i64))
+                } else {
+                    None
+                }
+            })
+        })
+        .collect()
 }
 
 fn main() {
-    let space = get_input();
-    println!("Part 1: {}", space.find_best());
+    let points = parse_input(include_str!("../input/input.txt"));
+
+    let (best_point, view_count) = find_best(&points);
+    println!("Part 1: {}", view_count);
+
+    let vaporized = vaporize(&points, &best_point, 200);
+    let point = vaporized[199];
+
+    println!("Part 2: {}", point.x * 100 + point.y);
 }
 
 #[cfg(test)]
@@ -138,7 +198,36 @@ mod tests {
 
     #[test]
     fn test_solution_part1() {
-        let space = get_input();
-        assert_eq!(space.find_best(), 286);
+        let points = parse_input(include_str!("../input/input.txt"));
+        assert_eq!(find_best(&points).1, 286);
+    }
+
+    #[test]
+    fn test_examples_part2() {
+        let points = parse_input(
+            ".#....#####...#..
+##...##.#####..##
+##...#...#.#####.
+..#.....#...###..
+..#.#.....#....##",
+        );
+        let vaporized = vaporize(&points, &Point::new(8, 3), 10);
+        assert_eq!(vaporized[0], Point::new(8, 1));
+        assert_eq!(vaporized[1], Point::new(9, 0));
+        assert_eq!(vaporized[2], Point::new(9, 1));
+        assert_eq!(vaporized[3], Point::new(10, 0));
+        assert_eq!(vaporized[4], Point::new(9, 2));
+        assert_eq!(vaporized[5], Point::new(11, 1));
+        assert_eq!(vaporized[6], Point::new(12, 1));
+        assert_eq!(vaporized[7], Point::new(11, 2));
+        assert_eq!(vaporized[8], Point::new(15, 1));
+    }
+
+    #[test]
+    fn test_solution_part2() {
+        let points = parse_input(include_str!("../input/input.txt"));
+        let vaporized = vaporize(&points, &Point::new(22, 25), 200);
+        let point = vaporized[199];
+        assert_eq!(point.x * 100 + point.y, 504);
     }
 }
